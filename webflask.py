8569d57691c60079da4939909b49067af451e2dc
Alexandre Saran <alexandre.saran@gmail.com>
2018-09-12 17:48:35 -0300

    mudanca6

--- webflask.py
+++ webflask.py
@@ -7,13 +7,13 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/'):
+@app.route('/')
 def hello():
     return 'Hello'
 
-@app.route('/main')
+@app.route('/main', methods= ['POST'])
 def home():
-    data = request.data
+    data = request.get_json(silent=True)["url_nota_fiscal"]
     return data
 
 if __name__ == '__main__':
