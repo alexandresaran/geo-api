76d2a6b3d03989fd25cc35726194d7c658e8411f
Alexandre Saran <alexandre.saran@gmail.com>
2018-09-12 17:22:18 -0300

    modificacao1

--- webflask.py
+++ webflask.py
@@ -1,9 +1,17 @@
-from flask import Flask
+import sys
+path = '/home/yourusername/mysite'
+if path not in sys.path:
+   sys.path.append(path)
+
+from flask_app import app as application
 from flask import request
 
 app = Flask(__name__)
 
-@app.route('/main')
-def Request():
+@app.route('/')
+def home():
     data = request.data
     return data
+
+if __name__ == '__main__':
+    app.run()
